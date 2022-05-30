c8e85089b2e60226cc48dee4c84bc5a4db864f65
yonialemye <yonialemye@gmail.com>
2022-05-30 23:20:49 +0300

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:covid_result_app/views/home_view.dart';
+import 'package:covid_result_app/views/verify_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
 import 'utils/colors.dart';
 import 'views/login_view.dart';
 import 'views/register_view.dart';
+import 'widgets/auth_views_widgets/logo_and_title.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +27,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: mainColor,
         fontFamily: 'Regular',
       ),
-      home: const RegisterView(),
+      home: const FirstScreenHandler(),
       onGenerateRoute: (RouteSettings settings) {
         switch (settings.name) {
           case RegisterView.routeName:
@@ -40,6 +45,44 @@ class MyApp extends StatelessWidget {
               duration: const Duration(milliseconds: 500),
               curve: Curves.easeOut,
               settings: settings,
+            );
+        }
+      },
+    );
+  }
+}
+
+class FirstScreenHandler extends StatelessWidget {
+  const FirstScreenHandler({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final User? user = FirebaseAuth.instance.currentUser;
+
+            if (user != null) {
+              if (user.emailVerified) {
+                return const HomeView();
+              } else {
+                return const VerifyView();
+              }
+            } else {
+              return const LoginView();
+            }
+          default:
+            return Scaffold(
+              body: Column(
+                children: const [
+                  SizedBox(height: 70),
+                  LogoAndTitle(),
+                  SizedBox(height: 20),
+                  CircularProgressIndicator(),
+                ],
+              ),
             );
         }
       },
---
+++ lib/views/verify_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VerifyView extends StatelessWidget {
+  const VerifyView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Verify view'),
+      ),
+    );
+  }
+}
