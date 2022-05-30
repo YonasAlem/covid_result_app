f77cf3824f0c85a4f29aa42e2d3e3ad5a706c2ae
yonialemye <yonialemye@gmail.com>
2022-05-30 18:23:19 +0300

    minimizing code

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -45,28 +45,7 @@ class _RegisterViewState extends State<RegisterView> {
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              const SizedBox(height: 80),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    logoSrc,
-                    height: 80,
-                  ),
-                  const SizedBox(width: 10),
-                  Text(
-                    appTitle,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Black',
-                      fontSize: 26,
-                      letterSpacing: 1,
-                      color: Colors.grey[800]!,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 70),
+              const LogoAndTitle(),
               Container(
                 padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 decoration: BoxDecoration(
@@ -76,7 +55,7 @@ class _RegisterViewState extends State<RegisterView> {
                   boxShadow: const [
                     BoxShadow(
                       color: Colors.black26,
-                      blurRadius: 10,
+                      blurRadius: 15,
                       spreadRadius: 1,
                       offset: Offset(0, 10),
                     ),
@@ -95,91 +74,42 @@ class _RegisterViewState extends State<RegisterView> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text(
-          'Create new account',
-          style: TextStyle(
-            letterSpacing: 1,
-            fontSize: 22,
-            fontFamily: 'Bold',
-          ),
-        ),
-        const SizedBox(height: 20),
-        const Text(
-          'Email address',
-          style: TextStyle(
-            color: Colors.grey,
-            letterSpacing: 1,
-          ),
-        ),
+        const SizedBox(height: 10),
+        const TextWidgetBig(text: 'Create new account'),
         const SizedBox(height: 5),
+        const Divider(
+          indent: 2,
+          endIndent: 260,
+          thickness: 3,
+          color: Colors.blue,
+        ),
+        const TextWidgetSmall(text: 'Email address'),
         AuthTextField(
           controller: _email,
           hintText: 'name@organization.domain',
         ),
-        const SizedBox(height: 10),
-        const Text(
-          'New password',
-          style: TextStyle(
-            color: Colors.grey,
-            letterSpacing: 1,
-          ),
-        ),
-        const SizedBox(height: 5),
+        const TextWidgetSmall(text: 'New password'),
         AuthTextField(
           controller: _password,
           isPassword: _password.text.isEmpty ? false : true,
         ),
-        const SizedBox(height: 10),
-        const Text(
-          'Confirm password',
-          style: TextStyle(
-            color: Colors.grey,
-            letterSpacing: 1,
-          ),
-        ),
-        const SizedBox(height: 5),
+        const TextWidgetSmall(text: 'Confirm password'),
         AuthTextField(
           controller: _confirmPassword,
           isPassword: _confirmPassword.text.isEmpty ? false : true,
         ),
         const SizedBox(height: 20),
-        Container(
-          height: 48,
-          width: double.maxFinite,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            gradient: gradient1,
-          ),
-          child: ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-                primary: Colors.transparent,
-                shadowColor: Colors.transparent,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                )),
-            child: const Text(
-              'Register',
-              style: TextStyle(
-                letterSpacing: 1,
-                fontFamily: 'Bold',
-              ),
-            ),
-          ),
+        SubmitButtonBig(
+          onTap: () {},
+          text: 'Register',
+          gradient: gradient1,
         ),
-        const SizedBox(height: 10),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Already have an account? ',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 1,
-              ),
-            ),
-            TextButton(
-              onPressed: () {
+            const TextWidgetSmall(text: 'Already have an account? '),
+            SubmitButtonSmall(
+              onTap: () {
                 Navigator.of(context).pushAndRemoveUntil(
                   MaterialPageRoute(
                     builder: (context) {
@@ -189,17 +119,146 @@ class _RegisterViewState extends State<RegisterView> {
                   (route) => false,
                 );
               },
-              child: const Text(
-                'Login Here.',
-                style: TextStyle(
-                  letterSpacing: 1,
-                  color: Colors.blue,
-                ),
-              ),
+              text: 'Login Here.',
+              context: context,
             ),
           ],
         ),
       ],
+    );
+  }
+}
+
+class LogoAndTitle extends StatelessWidget {
+  const LogoAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 80, bottom: 70),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/logo.png',
+            height: 80,
+          ),
+          const SizedBox(width: 10),
+          Text(
+            'Covid Result\nChecker',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Black',
+              fontSize: 26,
+              letterSpacing: 1,
+              color: Colors.grey[800]!,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TextWidgetBig extends StatelessWidget {
+  const TextWidgetBig({Key? key, required this.text}) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        letterSpacing: 1,
+        fontSize: 22,
+        fontFamily: 'Bold',
+      ),
+    );
+  }
+}
+
+class SubmitButtonSmall extends StatelessWidget {
+  const SubmitButtonSmall({Key? key, required this.context, required this.text, this.onTap})
+      : super(key: key);
+
+  final BuildContext context;
+  final String text;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onTap,
+      child: Text(
+        text,
+        style: const TextStyle(
+          letterSpacing: 1,
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
+
+class SubmitButtonBig extends StatelessWidget {
+  const SubmitButtonBig({Key? key, this.gradient, this.color, required this.text, this.onTap})
+      : super(key: key);
+
+  final LinearGradient? gradient;
+  final Color? color;
+  final String text;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      width: double.maxFinite,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: color,
+        gradient: gradient,
+      ),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          primary: Colors.transparent,
+          shadowColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            letterSpacing: 1,
+            fontFamily: 'Bold',
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextWidgetSmall extends StatelessWidget {
+  const TextWidgetSmall({Key? key, required this.text, this.fontSize}) : super(key: key);
+
+  final String text;
+  final double? fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 5),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.grey,
+          letterSpacing: 1,
+          fontSize: fontSize,
+        ),
+      ),
     );
   }
 }
