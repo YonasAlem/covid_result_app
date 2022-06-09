a5adbafa507b8ea9b30422e194c1b830be146518
yonialemye <yonialemye@gmail.com>
2022-06-09 16:36:09 +0300

    finish styling and when trainling icon tapped show dialog

--- lib/views/patient_list_view.dart
+++ lib/views/patient_list_view.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:covid_result_app/methods/my_app_bar.dart';
+import 'package:covid_result_app/methods/warning_dialog.dart';
 import 'package:covid_result_app/services/db_services/database_services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -109,45 +110,67 @@ class _PatientListViewState extends State<PatientListView> {
       itemBuilder: (context, index) {
         final PatientModel patient = patientList[index];
 
-        return ListTile(
-          onTap: () => singleListItem(patient: patient),
-          leading: Container(
-            margin: const EdgeInsets.all(5),
-            child: Image.asset(
-              patient.gender == 'Male' ? 'assets/images/male.png' : 'assets/images/female.png',
-              height: 35,
-            ),
-          ),
-          title: Text(
-            patient.fullName.toUpperCase(),
-            style: const TextStyle(letterSpacing: 1),
-          ),
-          subtitle: Row(
-            children: [
-              Text(
-                'Birthdate: ',
-                style: TextStyle(color: Colors.grey[300], fontSize: 12),
+        return Column(
+          children: [
+            ListTile(
+              onTap: () => singleListItem(patient: patient),
+              horizontalTitleGap: 15,
+              leading: Container(
+                margin: const EdgeInsets.all(5),
+                child: Image.asset(
+                  patient.gender == 'Male'
+                      ? 'assets/images/male.png'
+                      : 'assets/images/female.png',
+                  height: 35,
+                ),
               ),
-              Text(
-                patient.dateOfBirth,
-                style: const TextStyle(letterSpacing: 1, color: Colors.grey),
+              title: Text(
+                patient.fullName.toUpperCase(),
+                style: const TextStyle(letterSpacing: 1),
               ),
-            ],
-          ),
-          trailing: Container(
-            decoration: BoxDecoration(
-              color: const Color(0xFFdb7634),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: IconButton(
-              onPressed: () {},
-              constraints: const BoxConstraints(),
-              icon: Image.asset(
-                'assets/images/edit.png',
-                height: 35,
+              subtitle: Row(
+                children: [
+                  Text(
+                    'Birthdate: ',
+                    style: TextStyle(color: Colors.grey[300], fontSize: 12),
+                  ),
+                  Text(
+                    patient.dateOfBirth,
+                    style: const TextStyle(letterSpacing: 1, color: Colors.grey),
+                  ),
+                ],
+              ),
+              trailing: Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFdb7634),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: IconButton(
+                  onPressed: () async {
+                    final shouldUpdate = await warningDialog(
+                      context: context,
+                      boxTitle: "Update",
+                      boxDescription:
+                          "This will update the data from the server, Are you sure?",
+                      cancleText: "Cancel",
+                      okText: "Update",
+                    );
+                  },
+                  constraints: const BoxConstraints(),
+                  icon: Image.asset(
+                    'assets/images/edit.png',
+                    height: 35,
+                  ),
+                ),
               ),
             ),
-          ),
+            Divider(
+              height: 0,
+              thickness: 1,
+              indent: 75,
+              color: Colors.grey[200],
+            ),
+          ],
         );
       },
     );
@@ -155,85 +178,86 @@ class _PatientListViewState extends State<PatientListView> {
 
   singleListItem({required PatientModel patient}) {
     showModalBottomSheet(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
         ),
-        context: context,
-        builder: (context) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  color: Color(0xFFdb7634),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
+      ),
+      context: context,
+      builder: (context) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFFdb7634),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
                 ),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 20),
-                    Image.asset(
-                      patient.gender == 'Male'
-                          ? 'assets/images/male.png'
-                          : 'assets/images/female.png',
-                      height: 45,
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Image.asset(
+                    patient.gender == 'Male'
+                        ? 'assets/images/male.png'
+                        : 'assets/images/female.png',
+                    height: 45,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    patient.fullName.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 18,
+                      letterSpacing: 1,
                       color: Colors.white,
                     ),
-                    const SizedBox(height: 20),
-                    Text(
-                      patient.fullName.toUpperCase(),
-                      style: const TextStyle(
-                        fontSize: 18,
-                        letterSpacing: 1,
-                        color: Colors.white,
-                      ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'ID: @${patient.passportNumber}',
+                    style: const TextStyle(
+                      fontSize: 15,
+                      letterSpacing: 1,
+                      color: Colors.white54,
                     ),
-                    const SizedBox(height: 10),
-                    Text(
-                      'ID: @${patient.passportNumber}',
-                      style: const TextStyle(
-                        fontSize: 15,
-                        letterSpacing: 1,
-                        color: Colors.white54,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 20),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        TypeAndValueWidget(title: 'Result', value: patient.result),
-                        const SizedBox(width: 20),
-                        TypeAndValueWidget(
-                          title: 'Result taken date',
-                          value: patient.resultTakenDate.split('T')[0],
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        TypeAndValueWidget(title: 'Gender', value: patient.gender),
-                        const SizedBox(width: 20),
-                        TypeAndValueWidget(title: 'Date of birth', value: patient.dateOfBirth)
-                      ],
-                    ),
-                  ],
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      TypeAndValueWidget(title: 'Result', value: patient.result),
+                      const SizedBox(width: 20),
+                      TypeAndValueWidget(
+                        title: 'Result taken date',
+                        value: patient.resultTakenDate.split('T')[0],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      TypeAndValueWidget(title: 'Gender', value: patient.gender),
+                      const SizedBox(width: 20),
+                      TypeAndValueWidget(title: 'Date of birth', value: patient.dateOfBirth)
+                    ],
+                  ),
+                ],
               ),
-            ],
-          );
-        });
+            ),
+          ],
+        );
+      },
+    );
   }
 }
 
